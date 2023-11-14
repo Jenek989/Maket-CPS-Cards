1ddcd54dedafb05cbed46f051e6c3d0d4f5fc683
Eugene <62615682+Jenek989@users.noreply.github.com>
2023-11-14 04:08:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const slideList = document.querySelector('.swiper-wrapper');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  slideList.classList.toggle('hidden');
+  button.classList.toggle('button--open');
+  button.textContent = slideList.classList.contains('hidden') ? 'Скрыть' : 'Показать все';
+});
+
+let init = false;
+const swiperCard = () => {
+  if (window.innerWidth <= 767) {
+    if (!init) {
+      init = true;
+      swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 16,
+        width: 240,
+        centeredSlides: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    }
+  } else if (init) {
+    swiper.destroy();
+    init = false;
+  }
+}
+
+swiperCard();
+
+window.addEventListener("resize", () => {
+  swiperCard();
+  spoilerSlide();
+});
